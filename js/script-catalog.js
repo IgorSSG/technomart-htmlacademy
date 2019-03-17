5932c93501b21f9ca90ae8799e41108fab578081
YBLack <43601702+YBLack@users.noreply.github.com>
2019-03-17 23:16:40 +0300

    final-1

--- js/script-catalog.js
+++ js/script-catalog.js
@@ -12,3 +12,12 @@ buy.addEventListener("click", function (evt) {
   evt.preventDefault();
   cartPlus.classList.add("modal-show");
 });
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (cartPlus.classList.contains("modal-show")) {
+      cartPlus.classList.remove("modal-show")
+    }
+  }
+});
